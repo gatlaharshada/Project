e586e8009e372442be50582b7087a77ca16dfd5e
Harshada Gatla <gatlaharshada@gmail.com>
2025-12-29 19:04:13 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
+from .database import engine, Base
+from .routes import auth_routes, user_routes, admin_routes
 
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
+app.include_router(user_routes.router, prefix="/user", tags=["User"])
+app.include_router(admin_routes.router, prefix="/admin", tags=["Admin"])
