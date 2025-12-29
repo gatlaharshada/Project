659402751a3d6af0997cbc691d233d5759a5f42d
Harshada Gatla <gatlaharshada@gmail.com>
2025-12-29 18:59:38 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,12 @@
+from pydantic import BaseSettings
 
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
