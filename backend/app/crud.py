53f2f9a9e9d49f4f08863f2e1c2ab396cbbba665
Harshada Gatla <gatlaharshada@gmail.com>
2025-12-29 19:01:32 +0530

    Add user creation and retrieval functions

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,1 +1,13 @@
+from sqlalchemy.orm import Session
+from . import models, auth, schemas
 
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_pw = auth.hash_password(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_pw, is_admin=user.is_admin)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
