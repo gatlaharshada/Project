f2f742c08a3475b52733283dbf0328a58dd4ce03
Harshada Gatla <gatlaharshada@gmail.com>
2025-12-29 19:00:38 +0530

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
 
+class UserCreate(BaseModel):
+    email: str
+    password: str
+    is_admin: bool = False
+
+class UserOut(BaseModel):
+    id: int
+    email: str
+    is_admin: bool
+    class Config:
+        orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
